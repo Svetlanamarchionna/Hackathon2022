7baf3c2170cf4835ac42a7c29f35a40866b7a569
Svetlana <svetlana.marchionna@ithaka.org>
2022-02-17 05:17:53 -0500

    git commit

---
+++ data2.py
@@ -0,0 +1,67 @@
+data = [ 
+    {
+        "contenttitle": "Chemical Fallout: Rachel Carson's Silent Spring, Radioactive Fallout, and the Environmental Movement",
+        "contentid": "408b5054-9c03-321a-9dd3-8ce2963c1bac",
+        "doi": "10.2307/3984231",
+        "contentType": "journal",
+        "item type": "article",
+        "creator": "forest history society american society for environmental history oxford university press"
+    }
+    {
+        "contenttitle": "The Silent Spring of Rachel Carson",
+        "contentid": "020635bd-910e-3545-b7bb-337538fe947b",
+        "doi": "10.2307/25163080",
+        "contentType": "journal",
+        "item type": "article",
+        "creator": "Ned Kerstetter"
+    }
+    {
+        "contenttitle": "Turning the Tide: How Rachel Carson became a woman of letters",
+        "contentid": "a65a7582-f164-3a4c-ba05-234e084a4b56",
+        "doi": "10.2307/41222487",
+        "contentType": "journal",
+        "item type": "article",
+        "creator": "WILLIAM HOWARTH"
+    }
+    {
+        "contenttitle": "Heroines of Nature: Four Women Respond to the American Landscape",
+        "contentid": "8a6a5a40-aa8e-371f-8c70-cfa3128cd91c",
+        "doi": "10.2307/3984520",
+        "contentType": "journal",
+        "item type": "article",
+        "creator": "Vera L. Norwood"
+    }
+
+    #notes
+    {   "noteid": "9db3b637-9861-4ffc-8377-9766a9733795",
+        "creatoraccountid": "0e95b194-9577-44b0-a38c-9e3853a277aa",
+        "itemid": "5b52fe7d-1982-4562-8dd1-2f98af8a02d6",
+        "createdon": "2022-02-16 16:19:07.589978 +00:00",
+        "updatedon": "2022-02-16 16:19:07.589978 +00:00",
+        "note": "this article includes a portrait of rc near her home in maine. photo by erich hartmann, courtesy of the rachel carson council."
+    }
+    {   "noteid": "377efaa8-b32d-47ed-b6b1-961411e8f2fb",
+        "creatoraccountid": "0e95b194-9577-44b0-a38c-9e3853a277aa",
+        "itemid": "4d8e4724-57ac-4ea8-8948-1016f29ab427",
+        "createdon": "2022-02-16 16:19:52.190255 +00:00",
+        "updatedon": "2022-02-16 16:19:52.190255 +00:00",
+        "note": "photo of rc from 1951 on the first page of this article"
+    }
+    {   "noteid": "232dd864-d71d-4115-b807-4d343613eff3",
+        "creatoraccountid": "0e95b194-9577-44b0-a38c-9e3853a277aa",
+        "itemid": "7ea9c4c4-73c4-447e-8338-083038e015fd",
+        "createdon": "2022-02-16 16:20:52.676978 +00:00",
+        "updatedon": "2022-02-16 16:20:52.676978 +00:00",
+        "note": "p48 has photo of rc with dorothy freeman"
+    }
+    { 
+        "noteid": "ae07a61c-24c8-4845-bfd8-7c51fd0778ef",
+        "creatoraccountid": "0e95b194-9577-44b0-a38c-9e3853a277aa",
+        "itemid": "81587a07-9093-4a4b-ab49-46a4d7ab17ed",
+        "createdon": "2022-02-16 16:16:48.116124 +00:00",
+        "updatedon": "2022-02-16 16:16:48.116124 +00:00",
+        "note": "rachel carson featured in a peanuts comic"
+    }
+
+
+]
---
+++ notesfolder/notes.py
@@ -0,0 +1,40 @@
+#format for notes
+    "id": "",
+    "item_id": null,
+    "created_on": null,
+    "updated_on": null,
+    "creator_account_eid": null
+
+
+
+
+[
+  {
+    "id": 1,
+    "item_id": "5b52fe7d-1982-4562-8dd1-2f98af8a02d6",
+    "created_on": "2022-02-16 16:19:07.589978 +00:00",
+    "updated_on": "2022-02-16 16:19:07.589978 +00:00",
+    "creator_account_eid": "0e95b194-9577-44b0-a38c-9e3853a277aa"
+  },
+  {
+    "id": 2,
+    "item_id": "4d8e4724-57ac-4ea8-8948-1016f29ab427",
+    "created_on": "2022-02-16 16:19:52.190255 +00:00",
+    "updated_on": "2022-02-16 16:19:52.190255 +00:00",
+    "creator_account_eid": "0e95b194-9577-44b0-a38c-9e3853a277aa"
+  },
+  {
+    "id": 3,
+    "item_id": "7ea9c4c4-73c4-447e-8338-083038e015fd",
+    "created_on": "2022-02-16 16:20:52.676978 +00:00",
+    "updated_on": "2022-02-16 16:20:52.676978 +00:00",
+    "creator_account_eid": "0e95b194-9577-44b0-a38c-9e3853a277aa"
+  },
+  {
+    "id": 4,
+    "item_id": "81587a07-9093-4a4b-ab49-46a4d7ab17ed",
+    "created_on": "2022-02-16 16:16:48.116124 +00:00",
+    "updated_on": "2022-02-16 16:16:48.116124 +00:00",
+    "creator_account_eid": "0e95b194-9577-44b0-a38c-9e3853a277aa"
+  },
+]
